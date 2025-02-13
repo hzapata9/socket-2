537423720576da0e2263244bf6081a581979874d
hzapata9 <hzapata9@gmail.com>
2025-02-13 20:25:34 -0300

    Show room and message

--- public/app.js
+++ public/app.js
@@ -1,4 +1,42 @@
+const $form = document.querySelector("#form");
+const $message = document.querySelector("#message");
+const $messages = document.querySelector("#messages");
+let roomSelected = "";
+
 const socket = io("/chat");
-socket.on("connect", () => {
-  console.log("Connected to server");
+
+// enviar un mensaje a la sala room
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = $message.value;
+  if (message.trim() === "") {
+    alert("Escribe un mensaje");
+    return;
+  }
+
+  if (roomSelected === "") {
+    alert("Selecciona una sala");
+    return;
+  }
+
+  socket.emit("message", {
+    room: roomSelected,
+    message,
+  });
+  // $message.value = "";
+  $form.reset();
 });
+
+// escuchar los mensajes de la sala room1
+socket.on("message", (message) => {
+  $messages.innerHTML += `<li>${message}</li>`;
+});
+
+const room = (room) => {
+  console.log(room);
+  roomSelected = room;
+  // socket.emit("joinRoom", room);
+
+  // unirse a la sala room1
+  socket.emit("joinRoom", roomSelected);
+};
--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,34 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// namespaces
 const chat = io.of("/chat");
 
 chat.on("connection", (socket) => {
   console.log("User connected: ", socket.id);
+
+  // rooms
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    console.log("User joined room: ", room);
+
+    // enviar mensaje a todos los usuarios de la sala
+    chat.to(room).emit("message", `${socket.id} joined room: ${room}`);
+  });
+
+  // enviar mensaje a todos los usuarios de la sala
+  socket.on("message", ({ room, message }) => {
+    chat.to(room).emit("message", message);
+  });
+
+  // salir de la sala
+  socket.on("leaveRoom", (room) => {
+    socket.leave(room);
+    console.log("User left room: ", room);
+
+    // enviar mensaje a todos los usuarios de la sala
+    chat.to(room).emit("message", "User left room: " + room);
+  });
 });
 
 // static files
